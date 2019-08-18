b104337e0dc9321c66c96ad00923c30c7117cdca
Arina Victor <arinavictor@yahoo.com>
2019-08-18 17:49:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,12 @@ def reverse_each_word(ordered_string)
   
   ordered_array.each do |word|
     letter_array = word.split("")
-    reversed_word = letter_array.reverse.join("")
+    reversed_word = letter_array.reverse.join(" ")
     reversed_array << reversed_word
     
   end 
   
-  final = reversed_array.join("")
+  final = reversed_array.join(" ")
   final 
   
 end 
