ebc76ba32fa88dba80958fb59bc8380687c0aa0e
Arina Victor <arinavictor@yahoo.com>
2019-08-18 17:53:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -19,7 +19,7 @@ def reverse_each_word(ordered_string)
   reversed_array = []
   
   ordered_array.collect do |word|
-    letter_array = word.split(" ")
+    letter_array = word.split("")
     reversed_word = letter_array.reverse.join("")
     reversed_array << reversed_word
     
