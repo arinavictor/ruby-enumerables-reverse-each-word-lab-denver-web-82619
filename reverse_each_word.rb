5317c33d69a92542a8faf0c3be1a46906419a1ca
Arina Victor <arinavictor@yahoo.com>
2019-08-18 17:56:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,19 +15,18 @@ def reverse_each_word(ordered_string)
 end 
     
 def reverse_each_word(ordered_string)
-  ordered_array = ordered_string.split("")
+  ordered_array = ordered_string.split(" ")
   reversed_array = []
-  
-  ordered_array.collect do |word|
+
+   ordered_array.collect do |word|
     letter_array = word.split("")
     reversed_word = letter_array.reverse.join("")
     reversed_array << reversed_word
-    
   end
-  
+
   final = reversed_array.join(" ")
-  final
-end 
+ final
+end
     
     
     
