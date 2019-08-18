22d864709b44a8f7959ad22eeee5f61c9372539c
Arina Victor <arinavictor@yahoo.com>
2019-08-18 17:40:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
-def reverse_each_word(sentence)
-  sentence = "Hello there, and how are you?"
+def reverse_each_word(ordered_string)
+  ordered_array = "Hello there, and how are you?"
   sentence.chars
